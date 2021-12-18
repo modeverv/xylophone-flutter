fd0cf959e761ca08d6deec6612d1b1fca38cc270
seijiro <modeverv@gmail.com>
2021-12-18 21:47:39 +0900

    2021-12-18 21:47:39

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,74 @@
-import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  playLocal(int num) {
+    final player = AudioCache();
+    player.play("note$num.wav");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(child: Text(nouns.first)),
+          child: Center(
+            child: Column(
+              children: [
+                FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    playLocal(1);
+                  },
+                  child: Text('play 1'),
+                ),
+                FlatButton(
+                  color: Colors.blue,
+                  onPressed: () {
+                    playLocal(2);
+                  },
+                  child: Text('play 2'),
+                ),
+                FlatButton(
+                  color: Colors.orange,
+                  onPressed: () {
+                    playLocal(3);
+                  },
+                  child: Text('play 3'),
+                ),
+                FlatButton(
+                  color: Colors.yellow,
+                  onPressed: () {
+                    playLocal(4);
+                  },
+                  child: Text('play 4'),
+                ),
+                FlatButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    playLocal(5);
+                  },
+                  child: Text('play 5'),
+                ),
+                FlatButton(
+                  color: Colors.teal,
+                  onPressed: () {
+                    playLocal(6);
+                  },
+                  child: Text('play 6'),
+                ),
+                FlatButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    playLocal(7);
+                  },
+                  child: Text('play 7'),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
